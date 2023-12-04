69d101b2be2554b3cc6074128c50b14db72ca0ae
oaksoynl <143840597+oaksoynl@users.noreply.github.com>
2023-12-04 22:58:54 +0100

    Create solution_week9_ozgur.sql

---
+++ solution_week9_ozgur.sql
@@ -0,0 +1,3 @@
+İlk isminde Nick, Ed ve Jennifer adları bulunan aktörlerin adını ve soyadını gösterin.
+SELECT * FROM actor
+WHERE first_name = 'Nick' OR first_name = 'Ed' OR  first_name = 'Jennifer';
